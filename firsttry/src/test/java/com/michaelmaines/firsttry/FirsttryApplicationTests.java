0b2a6e52189c128ee95eefec954678e6f3a008b9
MichaelFSWebDev <101205737+michaelFSWebDev@users.noreply.github.com>
2022-03-09 13:40:03 -0500

    hi

---
+++ firsttry/src/test/java/com/michaelmaines/firsttry/FirsttryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelmaines.firsttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
